a8479c5aa2ecbdb89d13d50c1d90bab015571746
bhumika444 <83878217+bhumika444@users.noreply.github.com>
2026-05-01 22:52:27 -0400

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int climbStairs(int n) {
+        int one = 1, two = 1;
+        for(int i = n - 1; i > 0; i--){
+            int temp = one;
+            one = one + two;
+            two = temp;
+        }
+        return one;
+    }
+}
